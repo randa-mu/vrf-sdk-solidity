c296b1d72dc97ea827ea9577f3ed74a777414e08
Nemitari Ajienka <nemitariajienka@gmail.com>
2025-04-30 14:12:08 +0100

    remove MockSwapOrNotShuffleUser.sol

--- src/mocks/MockRandomnessConsumer.sol
+++ src/mocks/MockRandomnessConsumer.sol
@@ -4,13 +4,15 @@ pragma solidity 0.8.28;
 import {RandomUtils} from "../libraries/RandomUtils.sol";
 import {RandomIntXInRange} from "../libraries/RandomIntXInRange.sol";
 import {RandomUintXInRange} from "../libraries/RandomUintXInRange.sol";
+import {SwapOrNotShuffle} from "../libraries/SwapOrNotShuffle.sol";
 
 /// @title MockRandomLibsUser
 /// @notice Simple mock contract to expose library functions for testing.
-contract MockRandomLibsUser {
+contract MockRandomnessConsumer {
     using RandomUtils for uint256;
     using RandomIntXInRange for uint256;
     using RandomUintXInRange for uint256;
+    using SwapOrNotShuffle for uint64;
 
     function getRandomIndex(uint256 seed, uint64 size) external pure returns (uint64) {
         return seed.getRandomIndex(size);
@@ -34,5 +36,17 @@ contract MockRandomLibsUser {
 
     function getNextUintInRange(uint256 seed, uint256 min, uint256 max) external pure returns (uint256) {
         return seed.nextUintInRange(min, max);
+    }
+
+    function computeShuffledIndex(uint64 index, uint64 count, uint256 seed) external pure returns (uint64) {
+        return SwapOrNotShuffle.computeShuffledIndex(index, count, seed);
+    }
+
+    function shuffleIndices(uint64 count, uint256 seed) external pure returns (uint64[] memory) {
+        return SwapOrNotShuffle.shuffleIndices(count, seed);
+    }
+
+    function callBytesToUint64(bytes memory input) external view returns (uint64) {
+        return SwapOrNotShuffle.bytesToUint64(input);
     }
 }
